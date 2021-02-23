fdeb0f5ae603de127238a2325a7c1740563d46c6
Nurmanbetov <nurmanbetov.99@mail.ru>
2021-02-23 11:06:47 +0600

    added Good class

--- product/models.py
+++ product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Good(models.Model):
+    name = models.CharField(max_length=255) 
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(default=0)
+
+    def __str__(self):
+        return self.name
+
+  
